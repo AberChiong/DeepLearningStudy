b503fbfa02ce8d6a911abacc99b0c31e99750a30
cyb <able990731@outlook.com>
2023-04-25 13:30:48 +0800

    4.0线性分类器

--- 张量理解.py
+++ 张量理解.py
@@ -9,3 +9,18 @@ tensor4d[1, 2, 1, 3] = 1
 
 # 访问索引(1, 2, 1, 3)处的值
 print(tensor4d)
+
+import numpy as np
+
+# 创建一个大小为32x32x32的随机张量数据
+tensor_data = np.random.rand(32, 32, 32)
+
+# 输出张量的形状和数据类型
+print('Tensor shape:', tensor_data.shape)
+print('Tensor data type:', tensor_data.dtype)
+
+tensor_51 = np.random.rand(5, 1)
+tensor_15 = np.random.rand(1, 5)
+
+print(tensor_51)
+print(tensor_15)
